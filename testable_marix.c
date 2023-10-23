e3d5117216701060c2da04360ae1b40ba9039398
hapiwangy <99786569+hapiwangy@users.noreply.github.com>
2023-10-23 21:20:16 +0800

    Create testable_marix.c

---
+++ testable_marix.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    // store the size of matrix
+    int size;
+    while(1){
+        scanf("%d", &size);
+        // decide if size = 0
+        if(size){
+            // input the matrix
+            int matrix[size][size];
+            for(int i=0;i<size;i++)
+                for(int j=0;j<size;j++)
+                    scanf("%d",&matrix[i][j]);
+            //end of the input
+            int col=-1,row=-1;
+            // loop through the matrix's col
+            int count = 0;
+            while(count < size){
+                int test = 0;
+                for(int j=0;j<size;j++){
+                    test += matrix[count][j];
+                }
+                // check the test bit
+                if (test % 2 == 1){
+                    col = count;
+                }
+
+                count += 1;
+            }
+            // loop through the matrix's row
+            count = 0;
+            while(count < size){
+                int test = 0;
+                for(int j=0;j<size;j++){
+                    test += matrix[j][count];
+                }
+                // check the test bit
+                if (test % 2 == 1){
+                    row = count;
+                }
+                count += 1;
+            }
+            // check the matrix state
+            // state 1 everything is alright
+            if(col == -1 && row == -1) {printf("OK\n");}
+            else if((col == -1 && row != -1) || (row == -1 && col != -1)) {printf("Corrupt\n");}
+            else {printf("Change bit (%d,%d)\n",(col+1),(row+1));}
+        }
+        else {
+            // end of the input
+            break;
+        }
+    }
+    return 0;
+}
